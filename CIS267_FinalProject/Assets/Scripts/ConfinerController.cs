7e94c122389303895e29aeca63dd0239b7f854ea
imnotbrendandud <81782166+imnotbrendandud@users.noreply.github.com>
2021-11-08 18:20:23 -0500

    Update

--- CIS267_FinalProject/Assets/Scripts/ConfinerController.cs
+++ CIS267_FinalProject/Assets/Scripts/ConfinerController.cs
@@ -5,14 +5,14 @@ using UnityEngine.Tilemaps;
 
 public class ConfinerController : MonoBehaviour
 {
+    Transform t;
     public TilemapCollider2D tilemapCollider2D;
-
     Bounds mapBounds;
     PolygonCollider2D polygonCollider2D;
     // Start is called before the first frame update
     void Start()
     {
-
+        t = gameObject.GetComponent<Transform>();
         polygonCollider2D = gameObject.GetComponent<PolygonCollider2D>();
         mapBounds = tilemapCollider2D.bounds;
 
@@ -22,8 +22,8 @@ public class ConfinerController : MonoBehaviour
 
         colliderPoints = polygonCollider2D.points;
 
-        colliderPoints[0] = new Vector2(mapBounds.center.x - mapBounds.extents.x + 1, mapBounds.center.y + mapBounds.extents.y - 1);
-        colliderPoints[1] = new Vector2(mapBounds.center.x + mapBounds.extents.x - 1, mapBounds.center.y + mapBounds.extents.y - 1);
+        colliderPoints[0] = new Vector2(mapBounds.center.x - mapBounds.extents.x + Mathf.Abs(t.position.x) + 1, mapBounds.center.y + mapBounds.extents.y - 1 + Mathf.Abs(t.position.y));
+        colliderPoints[1] = new Vector2(mapBounds.center.x + mapBounds.extents.x - 1 - t.position.x, mapBounds.center.y + mapBounds.extents.y - 1 - t.position.y);
         colliderPoints[2] = new Vector2(mapBounds.center.x + mapBounds.extents.x - 1, mapBounds.center.y - mapBounds.extents.y + 1);
         colliderPoints[3] = new Vector2(mapBounds.center.x - mapBounds.extents.x + 1, mapBounds.center.y - mapBounds.extents.y + 1);
         polygonCollider2D.points = colliderPoints;
