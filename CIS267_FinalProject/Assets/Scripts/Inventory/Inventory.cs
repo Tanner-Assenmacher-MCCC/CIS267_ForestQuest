886c36e681ebed7935e5b0d66376cc3b9f11f9ed
jsadowyj0 <sadowyjj@gmail.com>
2021-11-08 17:20:36 -0500

    Starter Inventory & Player Prefab

--- CIS267_FinalProject/Assets/Scripts/Inventory/Inventory.cs
+++ CIS267_FinalProject/Assets/Scripts/Inventory/Inventory.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public static Inventory instance;
+    public static int maxItems = 24;
+    public List<GameObject> items = new List<GameObject>(maxItems);
+    private void Awake()
+    {
+        instance = this;
+    }
 
-//TEST 123456789
+    public bool IsFull()
+    {
+        return items.Count >= items.Capacity;
+    }
+
+    public bool AddItem(GameObject gameObject)
+    {
+        if (IsFull()) return false;
+        items.Add(gameObject);
+        return true;
+    }
+
+    public void RemoveItem(GameObject gameObject)
+    {
+        items.Remove(gameObject);
+    }
+
+    public void RemoveItem(int i)
+    {
+        items.RemoveAt(i);
+    }
+
 }
