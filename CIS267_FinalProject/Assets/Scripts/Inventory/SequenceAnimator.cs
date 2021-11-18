af06180ebcd62d7ccced8f63e0dcdacd6e5774ff
imnotbrendandud <81782166+imnotbrendandud@users.noreply.github.com>
2021-11-17 21:35:19 -0500

    HotbarSlotNumbers

--- CIS267_FinalProject/Assets/Scripts/Inventory/SequenceAnimator.cs
+++ CIS267_FinalProject/Assets/Scripts/Inventory/SequenceAnimator.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class SequenceAnimator : MonoBehaviour
 {
+    public GameObject inventory;
     List<Animator> animators;
     public float WaitBetween = 0.15f;
-    
+
     void Start()
     {
         animators = new List<Animator>(GetComponentsInChildren<Animator>());
-        StartCoroutine(DoAnimation());
     }
 
-   IEnumerator DoAnimation()
+    void Update()
     {
-        while(true)
+        if (!inventory.activeInHierarchy)
         {
-            foreach(var animator in animators)
+            StopCoroutine(DoAnimation());
+        }
+
+        else
+        {
+            StartCoroutine(DoAnimation());
+        }
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach (var animator in animators)
             {
                 animator.SetTrigger("DoAnimation");
                 yield return new WaitForSeconds(WaitBetween);
