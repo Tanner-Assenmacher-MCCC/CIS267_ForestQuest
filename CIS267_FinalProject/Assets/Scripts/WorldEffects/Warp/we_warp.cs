08fe7c01123a7ab44fa333dc42ae18fc5dcfb75c
kbartram03 <kbartram03@gmail.com>
2021-11-19 12:14:02 -0500

    Added Warps
    added warps and first caves

---
+++ CIS267_FinalProject/Assets/Scripts/WorldEffects/Warp/we_warp.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class we_warp : MonoBehaviour
+{
+    public bool down;
+
+    public GameObject target;
+
+    public GameObject fade;
+
+    private CircleCollider2D myCollider;
+
+    private static float exitAmount = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<CircleCollider2D>();
+        fade.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            fadeScreen(collision.gameObject);
+        }
+    }
+
+    private void fadeScreen(GameObject player)
+    {
+        fade.GetComponent<SpriteRenderer>().enabled = true;
+
+        warpPlayer(player);
+    }
+
+    private void warpPlayer(GameObject player)
+    {
+        if(target.GetComponent<we_warp>().exitDown())
+        {
+            player.transform.position = new Vector3(target.transform.position.x, target.transform.position.y - exitAmount, player.transform.position.z);
+        }
+        else
+        {
+
+        }
+
+
+    }
+
+    private void unfadeScreen()
+    {
+        fade.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    public bool exitDown()
+    {
+        return down;
+    }
+}
